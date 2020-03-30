59e151f7984320b885cda50f9b8b2a57a74bf82c
Zámbó <35877464+Dominosz1@users.noreply.github.com>
2020-03-30 11:56:14 +0200

    6 feladat
    6 feladat

---
+++ dolgozat0330/dolgozat0330/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dolgozat0330
+{
+    class Program
+    {
+        private static List<Versenyzo> versenyzok = new List<Versenyzo>();
+        static void Main(string[] args)
+        {
+            Beolvas();
+            Feladat3();
+            Feladat4();
+            Feladat6();
+            Console.ReadKey();
+        }
+
+        private static void Feladat6()
+        {
+            var v = versenyzok.Where(x => x.Kategoria.Contains("Noi")).OrderByDescending(x => x.Osszpontszam).First();
+            Console.WriteLine($"6. feladat: \n\tNév: {v.Nev}\n\tEgyesület: {v.Egyesulet}\n\tÖsszpont: {v.Osszpontszam}");
+        }
+
+        private static void Feladat4()
+        {
+            var nok = versenyzok.Count(n => n.Kategoria.Contains("Noi"));
+            var szazalek = (nok*100) / versenyzok.Count;
+            Console.WriteLine("Feladat 4: {0:0.00}%" , szazalek);
+        }
+
+        private static void Feladat3()
+        {
+            Console.WriteLine($"Feladat 3: {versenyzok.Count}");
+        }
+
+        private static void Beolvas()
+        {
+            var sr = new StreamReader(@"..\..\Res\fob2016.txt", Encoding.UTF8);
+            while (!sr.EndOfStream)
+            {
+                versenyzok.Add(new Versenyzo(sr.ReadLine()));
+            }
+            sr.Close();
+        }
+    }
+}
---
+++ dolgozat0330/dolgozat0330/Versenyzo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dolgozat0330
+{
+    class Versenyzo
+    {
+        public string Nev { get; set; }
+       public string Kategoria { get; set; }
+        public string Egyesulet { get; set;}
+         public int[] Pontok { get; set; }
+        public int Osszpontszam => Feladat5(Pontok);
+        public Versenyzo(string s)
+        {
+            var d = s.Split(';');
+            this.Nev = d[0];
+            this.Kategoria = d[1];
+            this.Egyesulet = d[2];
+            int[] tomb = new int[8];
+            for (int i = 0; i < 8; i++)
+            {
+                tomb[i] = int.Parse(d[i + 3]);
+            }
+            Pontok = tomb;
+
+        }
+        private int Feladat5(int[] pontok)
+        {
+            Array.Sort(pontok);
+            int osszpont = 0;
+            for (int i = 0; i < pontok.Length; i++)
+            {
+                if (pontok[i] != 0 && i < 2) pontok[i] = 10;
+                osszpont += pontok[i];
+            }
+            return osszpont;
+        }
+
+    }
+}
